fb8ccfcac54604056952bd5598450900f522ac31
unknown <Ketuanzee@gmail.com>
2026-03-07 20:24:05 +0100

    Added Emmasha page

--- views/Emmasha.php
+++ views/Emmasha.php
@@ -3,51 +3,50 @@
     <head>
         <title>MY PAGE</title>
 <style>
-    form {
-        width: 300px;
-        margin: 50px auto;
+    strong {
+        
     }
     body {
+        font-size: 1rem;
         background-color: #F5F5F5;
+        line-height: 1.4;
     }
 
-.Name{
-    text-align:center;
-}
-   div {
-    font-size: 15px;
-   } 
+.Name{text-align:center;
+    }
     </style>
     </head>
 <body>
     <div class="Name">
-    <img src="<?php echo plugin_dir_url(__FILE__); ?>images/jeff.png" alt="Kent Image" style="width:200px; height:200px; border-radius: 50%" ></div>
+    <img src="<?php echo plugin_dir_url(__FILE__); ?>images/jeff.png" alt="Kent Image" style="width:200px; height:200px; border-radius: 50%;" ></div>
    
-<h1 style="text-align:Center; font-size: 40px; background-color:#1e3a8a; float:center;  color: white; padding: 20px; border: 2px; border-radius: 10px"  >
+<h1 style="text-align:Center; font-size: 40px; background-color:#1e3a8a;  color: white; padding: 20px; border: 2px; border-radius: 10px"  >
 Welcome To My Personnal Page
 </h1><p>
-<b style="font-size: 30px; color: brown;"> About me</b>
-<hr><div>
+<b style="font-size: 2rem; color: brown;"> About me</b>
+<hr>
 I am an HND student at the University of Buea, reading Software Engineering. 
-I have a strong passion for coding and web development, where I enjoy turning ideas into functional and creative projects. I am also interested in plugin development, as it allows me to extend platforms and build tools that improve user experiences.  </div><p><br>
-<b style="font-size: 30px; color: brown;"> Sports</b><hr><div>
+I have a strong passion for coding and web development, where I enjoy turning ideas into functional and creative projects. I am also interested in plugin
+ development, as it allows me to extend platforms and build tools that improve user experiences. <p><br>
+<strong style="font-size: 2rem; color: brown;"> Sports</strong><hr>
     My best basketbal club LAKERS. <br> 
- Basketball is more than a sport to me. It teaches teamwork, focus, and discipline. Playing regularly helps me stay active and mentally sharp. </div><p> <br>
- <img src="<?php echo plugin_dir_url(__FILE__); ?>images/basketbal.png" alt="Kent Image" style="width:300px" ><br> <p>
- <div>Manchester city is my favourite football club </div><p><br>
+ Basketball is more than a sport to me. It teaches teamwork, focus, and discipline. Playing regularly helps me stay active and mentally sharp.<p> <br>
+ <img src="<?php echo plugin_dir_url(__FILE__); ?>images/basketbal.png" alt="Kent Image" style="width:300px" ><p><br> 
+ Manchester city is my favourite football club <br>
  <img src="<?php echo plugin_dir_url(__FILE__); ?>images/Emma.png" alt="Kent Image" style="width:300px" >
 <br>
 
- <b style="font-size: 30px; color: brown;"> What i'm working on</b><hr> <br> Right now, i am focused on
+ <strong style="font-size: 2rem; color: brown;"> What i'm working on</strong><hr> <br> Right now, i am focused on
 <ul style="list-style-type: disc; margin-left: 20px;">
     <li>Strengthening my programming skills</li>
    <li>Becoming better at web development</li>
 <li>Building projects that actually solve problems</li>
 </ul>
- <div class="Kent" style="background-color: #E6D3B3;"><p>
-    <b style="font-size: 30px; color: brown;">
-        Contact</b> <hr><br>
-       <b> Email: Ketuanzee@gmail.com<br> Tel: 237673698317<br>Location: Buea Cameroon</b>
-</div>
-<body>S
+ <div style="background-color: #E6D3B3;">
+    <strong style="font-size: 2rem; color: brown;">
+        Contact</strong> <hr><br>
+       <strong> Email: Ketuanzee@gmail.com<br> Tel: 237673698317<br>Location: Buea Cameroon</strong></div>
+<span>PHP</span>
+<button>Emmasha</button>
+</body>
 </html>
