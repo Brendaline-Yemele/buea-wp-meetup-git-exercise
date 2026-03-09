39aaa2161b235dd6360dfdbb39ae0343ea429c4f
Emmasha <Ketuanzee@gmail.com>
2026-03-09 11:43:16 +0100

    Modified program

--- views/Emmasha.php
+++ views/Emmasha.php
@@ -1,52 +1,66 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>MY PAGE</title>
-<style>
-    strong {
-        
-    }
-    body {
-        font-size: 1rem;
-        background-color: #F5F5F5;
-        line-height: 1.4;
-    }
+        <tittle></tittle>
+        <style>
+nav{border: 2px solid auto;
+    margin-top: 50px;
+    background: #2c4da7;
+    color: white;
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+}
+span {
+    
+gap:25px;
+}
 
-.Name{text-align:center;
-    }
-    </style>
-    </head>
-<body>
-    <div class="Name">
-    <img src="<?php echo plugin_dir_url(__FILE__); ?>images/jeff.png" alt="Kent Image" style="width:200px; height:200px; border-radius: 50%;" ></div>
-   
-<h1 style="text-align:Center; font-size: 40px; background-color:#1e3a8a;  color: white; padding: 20px; border: 2px; border-radius: 10px"  >
-Welcome To My Personnal Page
-</h1><p>
-<b style="font-size: 2rem; color: brown;"> About me</b>
-<hr>
-I am an HND student at the University of Buea, reading Software Engineering. 
-I have a strong passion for coding and web development, where I enjoy turning ideas into functional and creative projects. I am also interested in plugin
- development, as it allows me to extend platforms and build tools that improve user experiences. <p><br>
-<strong style="font-size: 2rem; color: brown;"> Sports</strong><hr>
-    My best basketbal club LAKERS. <br> 
- Basketball is more than a sport to me. It teaches teamwork, focus, and discipline. Playing regularly helps me stay active and mentally sharp.<p> <br>
- <img src="<?php echo plugin_dir_url(__FILE__); ?>images/basketbal.png" alt="Kent Image" style="width:300px" ><p><br> 
- Manchester city is my favourite football club <br>
- <img src="<?php echo plugin_dir_url(__FILE__); ?>images/Emma.png" alt="Kent Image" style="width:300px" >
-<br>
+.Name{     
+color: white;
+    background:linear-gradient(120deg,#2c4da7,#6b8cff);
+     display:flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
- <strong style="font-size: 2rem; color: brown;"> What i'm working on</strong><hr> <br> Right now, i am focused on
-<ul style="list-style-type: disc; margin-left: 20px;">
-    <li>Strengthening my programming skills</li>
-   <li>Becoming better at web development</li>
-<li>Building projects that actually solve problems</li>
-</ul>
- <div style="background-color: #E6D3B3;">
-    <strong style="font-size: 2rem; color: brown;">
-        Contact</strong> <hr><br>
-       <strong> Email: Ketuanzee@gmail.com<br> Tel: 237673698317<br>Location: Buea Cameroon</strong></div>
-<span>PHP</span>
-<button>Emmasha</button>
-</body>
-</html>
+.Name img{
+    
+width:180px;
+height:180px;
+border-radius:50%;
+border:6px solid white;
+}
+ 
+button {
+    background-color: #90EE90;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+    margin-left: 6px;
+    padding-left: 20px;
+      padding-right: 20px;
+}
+            </style>
+            </head>
+            <body>
+            
+                <nav>
+                <h2 style="color: white;"> KETUANZE EMMASHA NEMBUANYI </h2>
+                <ul>
+                    <span> Home</span>
+                    <span>About </span>
+</nav>
+
+<div class="Name">
+<div>
+<h1 style="color: white;"> Hi, i'm Emmasha</h1>
+<p>Aspiring Software Engineer</p>
+<p>
+I'm a passionate software engineering student at the University of Buea
+focusing on web development and plugin creation.
+</p>
+<button>Contact</button>
+</div>
+<img src="<?php echo plugin_dir_url(__FILE__); ?>images/jeff.png" alt="Kent Image" >
+</div>
