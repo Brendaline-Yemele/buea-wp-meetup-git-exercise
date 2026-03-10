0cb52927930ad70aed678487277afb5fd5ab79a6
Emmasha <Ketuanzee@gmail.com>
2026-03-10 12:44:12 +0100

    Modified Project completely

--- views/Emmasha.php
+++ views/Emmasha.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <tittle></tittle>
+        <title></title>
         <style>
-nav{border: 2px solid auto;
-    margin-top: 50px;
-    background: #2c4da7;
-    color: white;
-    display:flex;
-    justify-content: space-between;
-    align-items: center;
+            
+
+    nav{
+        background:#2c4da7;
+display:flex;
+justify-content:flex-start;
+align-items:center;
+
 }
-span {
-    
-gap:25px;
+
+    nav span{
+        color: white;
+margin-left:20px;
+cursor:pointer;
 }
 
 .Name{     
@@ -22,6 +25,7 @@ color: white;
      display:flex;
     justify-content: space-between;
     align-items: center;
+    padding: 10px;
 }
 
 .Name img{
@@ -30,37 +34,112 @@ width:180px;
 height:180px;
 border-radius:50%;
 border:6px solid white;
+
 }
- 
-button {
-    background-color: #90EE90;
+ .Name h1{
+color: white;
+}
+div button {
+    color:blue;
+    background-color: white;
     border: none;
     border-radius: 5px;
     padding: 10px;
-    margin-left: 6px;
-    padding-left: 20px;
-      padding-right: 20px;
+    
+    padding-left:15px;
+      padding-right: 15px;
+      
 }
+.About {
+    
+background:white;
+padding:25px;
+border-radius:10px;
+
+margin-bottom:20px;
+margin-top: 20px;
+
+}
+.merge {
+    display: flex;
+    justify-content: space-between;
+    gap: 10px;
+    margin: 10px;
+}
+
+.About button {
+     background-color: green;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+    
+    padding-left:15px;
+      padding-right: 15px;
+}
+
             </style>
             </head>
             <body>
             
                 <nav>
                 <h2 style="color: white;"> KETUANZE EMMASHA NEMBUANYI </h2>
-                <ul>
-                    <span> Home</span>
-                    <span>About </span>
+                
+                    <div>
+                       <span>Home</span>
+                       <span>About</span>
+                       <span>Projects</span>
+</div>
 </nav>
 
 <div class="Name">
 <div>
-<h1 style="color: white;"> Hi, i'm Emmasha</h1>
+<h1 > Hi, i'm Emmasha </h1>
 <p>Aspiring Software Engineer</p>
 <p>
 I'm a passionate software engineering student at the University of Buea
 focusing on web development and plugin creation.
 </p>
-<button>Contact</button>
+<button>Contact Me</button>
 </div>
 <img src="<?php echo plugin_dir_url(__FILE__); ?>images/jeff.png" alt="Kent Image" >
 </div>
+<div class="merge">
+    <div>
+<h2>About me</h2><hr>
+<p> I am an HND student at the University of Buea, reading Software Engineering.</p>
+<p>I have a strong passion for coding and web development, where I enjoy turning 
+ideas into functional and creative projects.</p>
+ <p>I am also interested in plugin
+ development, as it allows me to extend platforms and build tools that improve
+  user experiences.</p>
+  <div class="About">
+  <h2> What I'm Currently Working On</h2>
+  <ul style="list-style-type: disc; margin-left: 20px;">
+    <li>Strengthening my programming skills</li>
+   <li>Becoming better at web development</li>
+<li>Building projects that actually solve problems</li>
+</ul>
+
+</div>
+</div>
+
+<div class="About">
+    <h2> Experience</h2>
+   <p> <strong> Software Engineering Student </strong> <br>
+    2025 — present <br>
+    College Of Technology </p><br>
+    <strong> Web developement (learning)</strong><br>
+    Building projects in HTML, CSS and Wordpress
+
+    <h2> Contact </h2>
+   <strong> Email</strong>: Ketuanzee@gmail.com <br>
+   <p><strong> Location </strong>: Buea, Cameroon </P>
+   <button>Email</button>
+   <button>Whatsapp</button>
+   
+</div>
+</div>
+<div>
+    
+</body>
+</html>
